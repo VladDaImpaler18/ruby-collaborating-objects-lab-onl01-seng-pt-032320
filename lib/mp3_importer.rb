d33cfa9b3e34cd6f929192181fd6ad7635de1cd9
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-22 23:59:20 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,9 +1,12 @@
 class MP3Importer
   
+  attr_accessor :path
+  
+  
   def initialize(filepath)
   #Action Bronson - Larry Csonka - indie.mp3
   #Artist" - "Song" - "Genre.mp3
-  
+    binding.pry
   end
   
   def import
